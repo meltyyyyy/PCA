f64ec1c8f3d6c64628d75e682d166949ea7fcea7
takeru.abe <takeru.abe@gaiax.com>
2022-01-22 10:47:35 +0900

    pca heatmap

--- pca_dr/pca_dr.py
+++ pca_dr/pca_dr.py
@@ -21,7 +21,7 @@ def execute():
 
     plt.figure(figsize=(8, 8))
     mglearn.discrete_scatter(X_pca[:, 0], X_pca[:, 1], cancer.target)
-    plt.legend(cancer.target_names,loc='best')
+    plt.legend(cancer.target_names, loc='best')
     plt.gca().set_aspect('equal')
     plt.xlabel('First principal component')
     plt.ylabel('Second principal component')
@@ -30,3 +30,11 @@ def execute():
     print("PCA component shape: {}".format(pca.components_.shape))
     print("PCA components:\n{}".format(pca.components_))
 
+    plt.figure()
+    plt.matshow(pca.components_, cmap='viridis')
+    plt.yticks([0, 1], ["First component", "Second component"])
+    plt.colorbar()
+    plt.xticks(range(len(cancer.feature_names)), cancer.feature_names, rotation=60, ha='left')
+    plt.xlabel('Feature')
+    plt.ylabel('Principal components')
+    plt.savefig('pca_dr/pca_heatmap.png')
