c4d49b0e72ef2f74fd47c293e4f9e49f5932a35f
takeru.abe <takeru.abe@gaiax.com>
2022-01-22 11:27:01 +0900

    show shape of training data for eigenface

--- pca_eigenface/pca_eigenface.py
+++ pca_eigenface/pca_eigenface.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.datasets import fetch_lfw_people
+from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 
@@ -37,3 +38,10 @@ def execute():
     knn = KNeighborsClassifier(n_neighbors=1)
     knn.fit(X_train, y_train)
     print("Test set score of 1-nn: {:.2f}".format(knn.score(X_test, y_test)))
+
+    pca = PCA(n_components=100,whiten=True,random_state=0).fit(X_train)
+    X_train_pca = pca.transform(X_train)
+    X_test_pca = pca.transform(X_test)
+    print("X_train_pca.shape: {}".format(X_train_pca.shape))
+
+
